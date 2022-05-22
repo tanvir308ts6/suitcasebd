0df7312f779e962decfe3345a3ef24878afc101c
tanvir308ts6 <tanvir.eatlbd@gmail.com>
2022-05-23 00:38:44 +0600

    admin, customer, reseller module created

---
+++ src/app/reseller/reseller.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ResellerRoutingModule } from './reseller-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ResellerRoutingModule
+  ]
+})
+export class ResellerModule { }
